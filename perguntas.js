d4e03849ef4193dbb36edda3d4e74787c0ae25fc
vihhguima16 <vitoria.guimaraes.silva@escola.pr.gov.br>
2025-05-07 09:18:59 -0300

    07 do 05

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Anime',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da EUA?',
+    'A capital da EUA é Washington, D.C'
+)
+
+criaCartao(
+    '',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Animes',
+    'Qual a comida favorita do Naruto?',
+    'A comida favorita do Naruto é o Ramen.'
+)
